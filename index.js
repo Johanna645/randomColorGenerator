b989307b2c239a5662ace81a3bbb139946148694
Johanna Räihälä <johanna.raihala@gmail.com>
2021-01-19 17:52:26 +0100

    ask for color & print block

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
-const randomHex = require('random-hex-code');
+const randomColor = require('randomcolor'); // import the script
 
-const userInput = process.argv[2];
+console.log(process.argv[2]);
+const input = process.argv[2];
+let color = randomColor({ hue: input });
+console.log(color);
+function drawLongLine() {
+  let longStringOfHashtags = '';
+  let i = 0;
+  do {
+    longStringOfHashtags = longStringOfHashtags + '#';
+    i++;
+  } while (i < 31);
 
-randomHex.generate(); // #f92672
-randomHex.generate(); // #f060ff
+  console.log(longStringOfHashtags);
+}
+
+function drawShortLine() {
+  let shortStringOfHashtags = '';
+  let a = 0;
+  do {
+    if (a < 5 || a >= 26) {
+      shortStringOfHashtags = shortStringOfHashtags + '#';
+    } else {
+      shortStringOfHashtags = shortStringOfHashtags + ' ';
+    }
+    a++;
+  } while (a < 31);
+
+  console.log(shortStringOfHashtags);
+}
+
+function drawSpecialLine() {
+  let specialString = '';
+  let b = 0;
+  do {
+    if (b < 5 || b >= 26) {
+      specialString = specialString + '#';
+    } else if (b >= 5 || b < 12 || b >= 19 || b < 26) {
+      specialString = specialString + ' ';
+    } else {
+      // how do i get the color here? character at a time, string length color [0]
+    }
+    b++;
+  } while (b < 31);
+}
+
+drawLongLine();
+drawLongLine();
+drawLongLine();
+drawShortLine();
+
+drawSpecialLine();
+
+drawShortLine();
+drawLongLine();
+drawLongLine();
+drawLongLine();
+// const userInput = process.argv[2];
